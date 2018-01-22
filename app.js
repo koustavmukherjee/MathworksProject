3667c57507e52249c64de6628317cfaca3e4b3e3
Koustav Mukherjee <mukherjee.koustav@gmail.com>
2018-01-22 02:03:04 -0800

    Created a modal dialog for Custom Properties

--- app.js
+++ app.js
@@ -69,3 +69,79 @@ var parse = function() {
 	var classDef = Parser.parse(editorContents);
 	return classDef;
 }
+
+var logicalCheckboxValueChanged = function(lcb) {
+	if(lcb.checked) {
+		$("#PositiveIntegerCheckbox").attr("disabled", true);
+		$("#DependentCheckbox").attr("disabled", true);
+	}
+	else {
+		$("#PositiveIntegerCheckbox").attr("disabled", false);
+		$("#DependentCheckbox").attr("disabled", false);	
+	}
+}
+
+var nonTunableCheckboxValueChanged = function(ntcb) {
+	if(ntcb.checked) {
+		$("#DiscreteStateCheckbox").attr("disabled", true);
+	}
+	else {
+		$("#DiscreteStateCheckbox").attr("disabled", false);
+	}
+}
+
+var discreteStateCheckboxValueChanged = function(dvcb) {
+	if(dvcb.checked) {
+		$("#NontunableCheckbox").attr("disabled", true);
+		$("#ConstantCheckbox").attr("disabled", true);
+		$("#DependentCheckbox").attr("disabled", true);
+	}
+	else {
+		$("#NontunableCheckbox").attr("disabled", false);
+		$("#ConstantCheckbox").attr("disabled", false);
+		$("#DependentCheckbox").attr("disabled", false);
+	}
+}
+
+var positiveIntegerCheckboxValueChanged = function(picb) {
+	if(picb.checked) {
+		$("#LogicalCheckbox").attr("disabled", true);
+		$("#DependentCheckbox").attr("disabled", true);
+	}
+	else {
+		$("#LogicalCheckbox").attr("disabled", false);
+		$("#DependentCheckbox").attr("disabled", false);
+	}
+}
+
+var constantCheckboxValueChanged = function(cb) {
+	if(cb.checked) {
+		$("#DiscreteStateCheckbox").attr("disabled", true);
+		$("#DependentCheckbox").attr("disabled", true);
+	}
+	else {
+		$("#DiscreteStateCheckbox").attr("disabled", false);
+		$("#DependentCheckbox").attr("disabled", false);
+	}
+}
+
+var dependentCheckboxValueChanged = function(cb) {
+	if(cb.checked) {
+		$("#LogicalCheckbox").attr("disabled", true);
+		$("#DiscreteStateCheckbox").attr("disabled", true);
+		$("#PositiveIntegerCheckbox").attr("disabled", true);
+		$("#ConstantCheckbox").attr("disabled", true);
+	}
+	else {
+		$("#LogicalCheckbox").attr("disabled", false);
+		$("#DiscreteStateCheckbox").attr("disabled", false);
+		$("#PositiveIntegerCheckbox").attr("disabled", false);
+		$("#ConstantCheckbox").attr("disabled", false);
+	}
+}
+
+var insertCustomProperty = function() {
+	var hidden = $("#HiddenCheckbox").is(":checked") ? 1 : 0;
+	var constant = $("#ConstantCheckbox").is(":checked") ? 1 : 0;
+	var dependent = $("#DependentCheckbox").is(":checked") ? 1 : 0;
+}
